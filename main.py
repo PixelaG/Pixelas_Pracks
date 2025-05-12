10659a654f3e06d8678dcb747d63fad40ed396cf
PixelaG <143560379+PixelaG@users.noreply.github.com>
2025-05-12 06:00:46 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import os
+import re
+import time
+import discord
+import asyncio
+from dotenv import load_dotenv
+from discord.ext import commands
+from discord import app_commands, Intents
+from flask import Flask
+from threading import Thread
+from colorama import init, Fore
+from datetime import datetime, timedelta
+from pymongo import MongoClient
+
+
 import cxrameti
 import ocdaori
 import nolocdaati
 from bot_instance import bot
-
 
 load_dotenv()
 
