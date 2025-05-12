1e5255c3c21e71aa5838b7bad160a629fb8f56da
PixelaG <143560379+PixelaG@users.noreply.github.com>
2025-05-12 06:04:15 +0400

    Update bot_instance.py

--- bot_instance.py
+++ bot_instance.py
@@ -2,7 +2,9 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
+intents.members = True  
+intents.guilds = True
+intents.message_content = True 
+intents.messages = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
