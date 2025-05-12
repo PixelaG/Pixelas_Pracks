66a40ec6b2fe824945ecc6d54f8a3738f2f99c35
PixelaG <143560379+PixelaG@users.noreply.github.com>
2025-05-12 05:58:06 +0400

    Update bot_instance.py

--- bot_instance.py
+++ bot_instance.py
@@ -1,5 +1,16 @@
+import os
+import re
+import time
+import discord
+import asyncio
+from dotenv import load_dotenv
 from discord.ext import commands
-from discord import Intents
+from discord import app_commands, Intents
+from flask import Flask
+from threading import Thread
+from colorama import init, Fore
+from datetime import datetime, timedelta
+from pymongo import MongoClient
 
 intents = Intents.all()
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="p!", intents=intents)
